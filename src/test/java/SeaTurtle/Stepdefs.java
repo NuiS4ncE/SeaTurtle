7c1886b6f5940b96a0c41d2a94a13527a7031a3c
Teemu Oksanen <github@teemuoksanen.fi>
2020-11-22 16:47:43 +0200

    Changed Cucumber tests to use Book

--- src/test/java/SeaTurtle/Stepdefs.java
+++ src/test/java/SeaTurtle/Stepdefs.java
@@ -8,11 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.*;
 
-// Book ei ollut vielä käytössä, joten Book-oli toistaiseksi String.
-// Korvataan Book'illa, kun käytössä.
-
 public class Stepdefs {
-    List<String> testbooks;
+    List<Book> testbooks;
     
     @Given("Book list is initialized")
     public void bookListIsInitialized() {
@@ -21,7 +18,7 @@ public class Stepdefs {
 
     @When("a book title {string} is created")
     public void bookIsCreatedWithTitle(String title) {
-        String book = new String(title);
+        Book book = new Book(title);
         testbooks.add(book);
     }
 
