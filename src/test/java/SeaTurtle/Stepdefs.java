ba3257445ef3f052630442fe27793505bdeae33b
Johanna <jespoof@outlook.com>
2020-11-24 21:02:10 +0200

    fixed cucumber tests

--- src/test/java/SeaTurtle/Stepdefs.java
+++ src/test/java/SeaTurtle/Stepdefs.java
@@ -58,12 +58,12 @@ public class Stepdefs {
     }
 
     @Then("book's title {string} and page count of {string} is returned")
-    public void returnTitleAndPageCountAsString(String title, int pageCount) {
+    public void returnTitleAndPageCountAsString(String title, String pageCount) {
         assertEquals("Kirjan nimi: " + title + ". " + pageCount + " sivua.", testbooks.get(0).toString());
     }
 
     @Then("book's title {string}, author {string} and page count of {string} is returned")
-    public void returnAllDetailsAsString(String title, String author, int pageCount) {
+    public void returnAllDetailsAsString(String title, String author, String pageCount) {
         assertEquals("Kirjan nimi: " + title + ". Kirjoittaja: " + author + ". " + pageCount + " sivua.", testbooks.get(0).toString());
     }
 
