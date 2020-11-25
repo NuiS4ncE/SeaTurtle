0354dce4ae7d77a3d638cd666b4a3a4b8afedda8
Johanna <jespoof@outlook.com>
2020-11-25 20:43:24 +0200

    added bookmark to book class

--- src/main/java/SeaTurtle/Book.java
+++ src/main/java/SeaTurtle/Book.java
@@ -1,15 +1,19 @@
 package SeaTurtle;
 
+import SeaTurtle.ui.ConsoleColors;
+
 public class Book implements Comparable<Book>{
     
     private String title;
     private String author;
     private String pageCount;
+    private String bookmark;
     
     public Book(String title) {
         this.title = title;
         this.author = null;
         this.pageCount = null;
+        this.bookmark = null;
     }
     
     public Book(String title, String author, String pageCount) {
@@ -30,6 +34,10 @@ public class Book implements Comparable<Book>{
         return pageCount;
     }
     
+    public String getBookmark() {
+        return bookmark;
+    }
+    
     public void setAuthor(String author) {
         this.author = author;
     }
@@ -38,13 +46,28 @@ public class Book implements Comparable<Book>{
         this.pageCount = pageCount;
     }
     
+    public void setBookmark (String bookmark) {
+        this.bookmark = bookmark;
+    }
+    
     @Override
     public String toString() {
         StringBuilder bookDetails = new StringBuilder("Kirjan nimi: " + title + ".");
         if (author != null) {
             bookDetails.append(" Kirjoittaja: " + author + ".");
-        }if (pageCount != null) {
+        }if (pageCount != null ) {
             bookDetails.append(" "+ pageCount + " sivua.");
+        }if (bookmark != null) {
+            bookDetails.append(" Kirjanmerkki sivulla " + bookmark + ".");
+            int percentageRead = (int) Math.round(Integer.parseInt(bookmark) * 100.0/Integer.parseInt(pageCount));
+            bookDetails.append(" Kirjasta luettu ");
+            if(percentageRead == 0) {
+                bookDetails.append(ConsoleColors.RED + "0 %." + ConsoleColors.RESET);
+            } else if (percentageRead == 100) {
+                bookDetails.append(ConsoleColors.GREEN + "100 %." + ConsoleColors.RESET);
+            } else {
+                bookDetails.append(ConsoleColors.YELLOW + percentageRead + " %." + ConsoleColors.RESET);
+            }
         }
         return bookDetails.toString();
     }
