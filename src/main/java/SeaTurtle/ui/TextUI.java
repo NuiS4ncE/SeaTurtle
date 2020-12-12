da873f29b2fb74be79f6788ec9e3d3cb20be2b69
Teemu Oksanen <16786317+teemuoksanen@users.noreply.github.com>
2020-12-12 11:11:29 +0200

    Removed unused logging imports

--- src/main/java/SeaTurtle/ui/TextUI.java
+++ src/main/java/SeaTurtle/ui/TextUI.java
@@ -8,8 +8,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.commons.validator.UrlValidator;
 
 public class TextUI {
